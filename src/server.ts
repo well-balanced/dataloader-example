2e115f5feaeeb706b33d5a3d6cde5d7b156e18bf
Woosik Kim <iam.woosik.kim@gmail.com>
2022-08-12 19:54:22 +0900

    :wrench: Watch query events

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 import { ApolloServer } from 'apollo-server'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 import { resolvers, typeDefs } from './resolvers'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ log: ['query'] })
+
+prisma.$on<any>('query', async (e: Prisma.QueryEvent) => {
+  console.log(`${e.query} ${e.params}`)
+})
 
 const server = new ApolloServer({
   schema: makeExecutableSchema({ typeDefs, resolvers }),
